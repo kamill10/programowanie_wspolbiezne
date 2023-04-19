7acfa4f988aad2cdec3eb1d50c07628116f0da9f
242425 <242425@edu.p.lodz.pl>
2023-04-19 17:50:11 +0200

    warstwa dane

--- BallsCollisions/Logic/LogicAbstractApi.cs
+++ BallsCollisions/Logic/LogicAbstractApi.cs
@@ -1,5 +1,4 @@
-﻿using System.Numerics;
-using Data;
+﻿using Data;
 
 namespace Logic
 {
--- BallsCollisions/Presentation/ViewModel/ViewApi.cs
+++ BallsCollisions/Presentation/ViewModel/ViewApi.cs
@@ -1,12 +1,6 @@
-﻿using System.Collections;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
 using Logic;
 using Presentation.Model;
 
